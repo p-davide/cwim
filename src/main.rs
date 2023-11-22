1377a78217375a4ef02008f8aa7082626cf38a47
Davide Panaroni <thepana00@gmail.com>
2023-11-22 22:35:30 +0100

    Parser: Implemented parsing of integers

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,14 @@ enum TokenType {
     Comma,
 }
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
-struct Token {
+struct Token<'t> {
     ttype: TokenType,
-    length: usize,
+    lexeme: &'t str,
 }
 
 struct ParseState<'a> {
     to_parse: &'a str,
-    tokens: Vec<Token>,
+    tokens: Vec<Token<'a>>,
 }
 
 type ParseError = ();
@@ -34,7 +34,7 @@ fn parse(text: &str) -> Result<Vec<Token>, ParseError> {
         match parse_token(state.to_parse) {
             Ok(token) => {
                 state.tokens.push(token);
-                state.to_parse = &state.to_parse[token.length..]
+                state.to_parse = &state.to_parse[token.lexeme.len()..]
             }
             Err(err) => return Err(err)
         }
@@ -43,13 +43,32 @@ fn parse(text: &str) -> Result<Vec<Token>, ParseError> {
 }
 
 fn parse_token(text: &str) -> Result<Token, ParseError> {
-    let it = Token {
-        ttype: TokenType::Space,
-        length: 1,
-    };
-    Ok(it)
+    for c in text.chars() {
+        match c {
+            '0'..='9' => return parse_number(text),
+            _ => { break; }
+        }
+    }
+    Err(())
 }
 
+fn parse_number(text: &str) -> Result<Token, ParseError> {
+    let mut l: usize = 0;
+    for c in text.chars() {
+        if c.is_ascii_digit() {
+            l += 1;
+        } else { break; }
+    }
+    if l == 0 {
+        Err(())
+    } else {
+        let token = Token {
+            ttype: TokenType::Literal,
+            lexeme: &text[..l]
+        };
+        Ok(token)
+    }
+}
 fn main() {
-    println!("{:?}", parse("lmao"));
+    println!("{:?}", parse("234"));
 }
