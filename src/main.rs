983048f1f835365e68fbf9dcecadf6ebf4861576
Davide Panaroni <thepana00@gmail.com>
2024-02-15 11:54:31 +0100

    Replaced `inquire` crate with `rustyline`

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::io::IsTerminal;
 
 use cwim::env::*;
 use cwim::interpreter::*;
-use inquire::Text;
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
 
 fn run_line(line: &str, env: &mut Env) {
     match run(&line, env) {
@@ -15,17 +16,41 @@ fn run_line(line: &str, env: &mut Env) {
     }
 }
 
-fn repl() {
+fn repl() -> Result<()> {
     let mut env = Env::prelude();
-    while let Ok(line) = Text::new("").prompt() {
-        run_line(&line, &mut env);
+    let mut rl = DefaultEditor::new()?;
+    #[cfg(feature = "with-file-history")]
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
     }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                let _ = rl.add_history_entry(line.as_str());
+                run_line(&line, &mut env);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    Ok(())
 }
 
 fn main() {
     let stdin = io::stdin();
     if stdin.is_terminal() {
-        repl()
+        repl();
     } else {
         let mut env = Env::prelude();
         for line in stdin.lines() {
