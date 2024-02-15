273ef9967335796b0794ff583c6f918fed15b1a3
Davide Panaroni <thepana00@gmail.com>
2024-02-15 11:11:42 +0100

    Main: accept piped input

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
+use std::io;
+use std::io::IsTerminal;
+
 use cwim::env::*;
 use cwim::interpreter::*;
 use inquire::Text;
 
-fn main() {
+fn run_line(line: &str, env: &mut Env) {
+    match run(&line, env) {
+        Ok(result) => {
+            println!("{}", result);
+            env.assign("ans".to_owned(), Variable::Value(result));
+        }
+        Err(msg) => eprintln!("{}", msg),
+    }
+}
+
+fn repl() {
     let mut env = Env::prelude();
     while let Ok(line) = Text::new("").prompt() {
-        match run(&line, &mut env) {
-            Ok(result) => {
-                println!("{}", result);
-                env.assign("ans".to_owned(), Variable::Value(result));
-            }
-            Err(msg) => eprintln!("{}", msg),
+        run_line(&line, &mut env);
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    if stdin.is_terminal() {
+        repl()
+    } else {
+        let mut env = Env::prelude();
+        for line in stdin.lines() {
+            run_line(&line.expect("no line found"), &mut env);
         }
     }
 }
